bb6b6842f8f8ab658d00d07d81e6ea8e9c6ffbde
Jackarivera <jackarivera@gmail.com>
2020-02-05 21:31:57 -0600

    Added Speed Variables; Added Climbing Ports

--- src/main/java/frc/robot/Variables.java
+++ src/main/java/frc/robot/Variables.java
@@ -2,13 +2,23 @@ package frc.robot;
 
 public class Variables {
 
+    // Motor Ports
     public static final int frontLeftMotorPort = 1;
-    public static final int frontRightMotorPort = 0;
-    public static final int backLeftMotorPort = 2;
-    public static final int backRightMotorPort = 3;
-    public static final int conveyorMotorPort = 0;
-    public static final int feedMotorPort = 1;
-    public static final int shooterMotorOnePort = 2;
-    public static final int shooterMotorTwoPort = 3;
-    public static final int intakeMotorPort = 8;
+    public static final int frontRightMotorPort = 2;
+    public static final int backLeftMotorPort = 3;
+    public static final int backRightMotorPort = 4;
+    public static final int conveyorMotorPort = 5;
+    public static final int feedMotorPort = 6;
+    public static final int shooterMotorOnePort = 7;
+    public static final int shooterMotorTwoPort = 8;
+    public static final int intakeMotorPort = 9;
+    public static final int raiseMotorPort = 10;
+    public static final int climbMotorPort = 11;
+
+    // Speed Amounts
+    public static final double intakeMotorSpeed = 0.25;
+    public static final double conveyorIndexSpeed = 1.0;
+    public static final double feedIndexSpeed = 1.0;
+    public static final double raiseMotorSpeed = 1.0;
+    public static final double climbMotorSpeed = 1.0;
 }
