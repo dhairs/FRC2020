5323dc91fdf6c18072d3df7312e1f2bad6816def
Jackarivera <jackarivera@gmail.com>
2020-02-05 23:52:14 -0600

    Created a Shuffleboard Wrapper

---
+++ src/main/java/frc/robot/ShuffleboardWrapper.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+public class ShuffleboardWrapper {
+    public ShuffleboardWrapper() {
+        
+    }
+
+    public void setWidget(String sbTab, String widgetTitle, Object defaultValue, BuiltInWidgets widgetType){
+        Shuffleboard.getTab(sbTab).add(widgetTitle, defaultValue).withWidget(widgetType).getEntry();
+    }
+
+}
